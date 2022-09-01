2d8863eea666e4f1e1594b4a153a977fd89f0650
hyun <hyun.namgung@desilo.ai>
2022-09-01 19:30:52 +0900

    check

--- monorepo/client/asker/src/App.js
+++ monorepo/client/asker/src/App.js
@@ -1,4 +1,5 @@
 import multiply from 'common/utils/multiply';
+import dayjs from 'dayjs';
 // import Header from 'common/components'; // TODO
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       {/* <Header /> */}
       <div>FE1</div>
       <div>4 x 5 = {multiply(3, 5)}</div>
+      <div>{dayjs().toISOString()}</div>
     </div>
   );
 }
